20c6ef1b27a9c8f73c6549de6e550885edf2af47
dolphintwo <dtdinghui@126.com>
2019-03-29 18:23:26 +0800

    arg2

---
+++ basics/02/03/test-demo-args2.go
@@ -0,0 +1,29 @@
+//go run test-demo-args2.go -name="Dinphin"
+//go run test-demo-args2.go --help
+package main
+
+import (
+	"flag"
+	"fmt"
+	"os"
+)
+
+var name string
+
+func init() {
+	flag.CommandLine = flag.NewFlagSet("", flag.ExitOnError)
+	flag.CommandLine.Usage = func() {
+		fmt.Fprintf(os.Stderr, "Usage of %s:\n", "question")
+		flag.PrintDefaults()
+	}
+	flag.StringVar(&name, "name", "everyone", "The greeting object.")
+}
+
+func main() {
+	// flag.Usage = func() {
+	// 	fmt.Fprintf(os.Stderr, "Usage of %s:\n", "question")
+	// 	flag.PrintDefaults()
+	// }
+	flag.Parse()
+	fmt.Printf("Hello,%s!\n", name)
+}
